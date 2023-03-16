c8cc93d800f0b7cf53370228ed7afcc5023bf3ed
mrendy58 <derendyyy58@gmail.com>
2023-03-16 20:31:57 +0700

    prosedur unsorted

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -36,6 +36,17 @@ void bubbleSortArray() {                        //procedur untuk mengurutkan arr
         }
     }
 }
+
+void unsorted() {
+    cout << endl;                              //output baris kosong
+    cout << "==================================" << endl;  //output ke layar
+    cout << "Element Array yang telah tersusun" << endl;  //output ke layar
+    cout << "==================================" << endl; //output ke layar
+    for (int j = 0; j < n; j++) {               //looping dengan j dimulai dari 0 hingga n-1
+        cout << a[j] << endl;                   // output ke layar
+    }
+    cout << endl;
+}
 int main()                   
 {
     std::cout << "Hello World!\n";
